3b7de9b6d3ff089fcfcf8fe0f6d7dc17c92cb96c
Samar Srivastav <samsri156@gmail.com>
2022-04-12 18:03:28 +0530

    Time: 1 ms (55.24%), Space: 42.2 MB (58.24%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,28 +1,26 @@
 class Solution {
+    public int getLive(int[][] board,int i,int j){
+        int l=0;
+        if(i-1>=0 && j-1>=0 && board[i-1][j-1]==1) l++;
+        if(i+1<board.length && j+1<board[0].length && board[i+1][j+1]==1) l++;
+        if(i-1>=0 && j+1<board[0].length && board[i-1][j+1]==1) l++;
+        if(i+1<board.length && j-1>=0 && board[i+1][j-1]==1) l++;
+        if(i-1>=0&& board[i-1][j]==1) l++;
+        if(i+1<board.length&& board[i+1][j]==1) l++;
+        if(j-1>=0 && board[i][j-1]==1) l++;
+        if(j+1<board[0].length && board[i][j+1]==1) l++;
+        return l;
+    }
     public void gameOfLife(int[][] board) {
         int[][] aux=new int[board.length][board[0].length];
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 int l=0;
                 if(board[i][j]==0){
-                    if(i-1>=0 && j-1>=0 && board[i-1][j-1]==1) l++;
-                    if(i+1<board.length && j+1<board[0].length && board[i+1][j+1]==1) l++;
-                    if(i-1>=0 && j+1<board[0].length && board[i-1][j+1]==1) l++;
-                    if(i+1<board.length && j-1>=0 && board[i+1][j-1]==1) l++;
-                    if(i-1>=0&& board[i-1][j]==1) l++;
-                    if(i+1<board.length&& board[i+1][j]==1) l++;
-                    if(j-1>=0 && board[i][j-1]==1) l++;
-                    if(j+1<board[0].length && board[i][j+1]==1) l++;
+                    l=getLive(board,i,j);
                     if(l==3) aux[i][j]=1;
                 }else{
-                    if(i-1>=0 && j-1>=0 && board[i-1][j-1]==1) l++;
-                    if(i+1<board.length && j+1<board[0].length && board[i+1][j+1]==1) l++;
-                    if(i-1>=0 && j+1<board[0].length && board[i-1][j+1]==1) l++;
-                    if(i+1<board.length && j-1>=0 && board[i+1][j-1]==1) l++;
-                    if(i-1>=0&& board[i-1][j]==1) l++;
-                    if(i+1<board.length&& board[i+1][j]==1) l++;
-                    if(j-1>=0 && board[i][j-1]==1) l++;
-                    if(j+1<board[0].length && board[i][j+1]==1) l++;
+                    l=getLive(board,i,j);
                     if(l<2) aux[i][j]=0;
                     else if(l==2 || l==3) aux[i][j]=1;
                     else aux[i][j]=0;
